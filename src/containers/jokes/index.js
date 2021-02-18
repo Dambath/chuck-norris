7a894e2d23ed8a0bb9b593e55e65a4ea392fb0eb
Dambath <dvaliyev16@gmail.com>
2021-02-19 00:06:59 +0600

    final changes

--- src/containers/jokes/index.js
+++ src/containers/jokes/index.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Joke from '../../components/card/index'
 import '../jokes/style.css'
-import { Select } from 'antd'
 import chuckImage from '../../chuck.jpe'
 
 function Main() {
