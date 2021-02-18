2de50b1c1d723950e5f6bb81edd21c32da175c49
Dambath <dvaliyev16@gmail.com>
2021-02-18 23:01:32 +0600

    deploy my react app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Main from './containers/jokes/index'
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path='/jokes' component={Main} /> 
-      </Switch>
-    </Router>
+    <Main/>
   )
 }
 
