46c33b1248575cd9170df73c81b4990646f1c333
Dambath <dvaliyev16@gmail.com>
2021-02-18 23:21:09 +0600

    last version

--- src/containers/jokes/index.js
+++ src/containers/jokes/index.js
@@ -4,28 +4,19 @@ import Joke from '../../components/card/index'
 import '../jokes/style.css'
 import { Select } from 'antd'
 import chuckImage from '../../chuck.jpe'
-const {Option} = Select
 
 function Main() {
     
     const [jokes, setJokes] = useState([])
-    const [category, setCategory] = useState([])
 
     useEffect(() => {
         generateJoke()
-        fetchCategory()
     }, [])
 
     const jokesItems = jokes.map((item, i) => <Joke id={item.id} value={item.value} icon={item.icon_url}/>)
 
-    const categoryitems = category.map((item, i) => <Option>{item[i]}</Option>)
-
     const generateJoke = () => {
         axios.get('https://api.chucknorris.io/jokes/random').then(res => setJokes([res.data]))
-    }
-
-    const fetchCategory = () => {
-        axios.get('https://api.chucknorris.io/jokes/categories').then(res => setCategory(res.data))
     }
 
     return (
